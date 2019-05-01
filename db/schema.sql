efea7d4b6593d6629ff2f7514df2791c273fcd0d
mike4747 <mike4747@oh.rr.com>
2019-05-01 00:14:19 -0400

    ready to push changes to heroku

--- db/schema.sql
+++ db/schema.sql
@@ -1,30 +1,34 @@
+-- DROP DATABASE IF EXISTS burgers_db;
+-- CREATE DATABASE burgers_db;
+-- USE burgers_db;
+
 CREATE TABLE burgers (
     id INT AUTO_INCREMENT NOT NULL,
     patty_id int(3) NOT NULL,
     bun_id int(3) NOT NULL,
     topping_id int(3) NOT NULL,
     devoured boolean DEFAULT 0,
-    createdAt TIMESTAMP NOT NULL,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE patties (
     patty_id INT AUTO_INCREMENT NOT NULL,
     patty varchar(30) NOT NULL,
-    createdAt TIMESTAMP NOT NULL,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY Key(patty_id)
 );
 
 CREATE TABLE buns (
     bun_id INT AUTO_INCREMENT NOT NULL,
     bun varchar(30) NOT NULL,
-    createdAt TIMESTAMP NOT NULL,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY Key(bun_id)
 );
 
 CREATE TABLE toppings (
     topping_id INT AUTO_INCREMENT NOT NULL,
     topping varchar(30) NOT NULL,
-    createdAt TIMESTAMP NOT NULL,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY Key(topping_id)
 );
